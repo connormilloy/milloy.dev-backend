0dfcc0e827b70c22e2781fef40db33ac4586a40c
connor-milloy <connor.milloy@skyscanner.net>
2025-08-04 19:00:00 +0100

    Add validation middleware

--- src/routes/trains/trains.js
+++ src/routes/trains/trains.js
@@ -1,25 +1,23 @@
 const express = require('express');
-
+const { validateParameters } = require('./utils/middlewares');
 const router = express.Router();
-
-router.get('/get-next-train-from-origin-to-destination/:origin/:destination', (req, res) => {
+router.get(
+  '/get-next-from-origin-destination/:origin/:destination',
+  validateParameters,
+  (req, res) => {
     const { origin, destination } = req.params;
-
-    if (!origin || !destination) {
-        return res.status(400).json({ error: 'Origin and destination are required.' });
-    }
 
     // Simulate fetching train data
     const trainData = {
-        origin,
-        destination,
-        nextTrain: '12:30 PM',
-        duration: '1 hour 15 minutes',
-        price: '£25.00',
+      origin,
+      destination,
+      nextTrain: '12:30 PM',
+      duration: '1 hour 15 minutes',
+      price: '£25.00',
     };
 
     res.json(trainData);
-});
-
+  }
+);
 
 module.exports = router;
